630878065a21f55b828ba65b949c4fb234bb21eb
Your Name <you@example.com>
2024-05-31 16:18:45 +0300

    Fix5

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,12 +1,13 @@
-#include "Server.hpp"
+//#include "Server.hpp"
 #include <boost/asio.hpp>
+#include "iostream"
 
 int main(){
 
     boost::asio::io_service io_service;
-    VoxelServer voxel(io_service);
-
-    io_service.run();
-
+    
+    //  VoxelServer voxel(io_service);
+    //io_service.run();
+    std::cout << &io_service;
     return 0;
 }
